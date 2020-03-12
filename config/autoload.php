76975bad3c1ef383f6408cfde206846d91065981
supham <supalpuket@gmail.com>
2020-03-12 23:35:07 +0700

    Perbaikan error blank screen

--- config/autoload.php
+++ config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array(WPSID::config('ci_app_path'));
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array();
 
 /*
 | -------------------------------------------------------------------
